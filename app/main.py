7dd32abf28c36aa4834df52d516a9c116b52c569
Matheus Marques <matheusmarquesportela@gmail.com>
2026-03-03 19:59:36 -0300

    Atualização para testar a API no Postman

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI 
+from app.modules.clients.router import router as clients_router
 
 app = FastAPI(
     title="API do Powp - System Enterprise",
     description="API para o sistema ERP voltado ao projeto final de curso",
     version="1.0.0"
 )
+
+app.include_router(clients_router)
 
 @app.get("/")
 def health_check():
