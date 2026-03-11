e5996c7362ce6be027436e65ea57655c007c64a3
Matheus Marques <matheusmarquesportela@gmail.com>
2026-03-10 21:29:41 -0300

    Função GET para listar os clientes

--- app/modules/clients/router.py
+++ app/modules/clients/router.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 from .schemas import ClienteCreate
-from .services import create_cliente
+from .services import create_cliente, getCliente_paginate
 
 router = APIRouter(prefix="/clientes", tags=["Clientes"])
 
 @router.post("/", status_code=201)
 def cadastrar_cliente(cliente: ClienteCreate):
     return create_cliente(cliente)
+
+@router.get("/", status_code=201)
+def listar_clientes():
+    return getCliente_paginate()
--- app/modules/clients/services.py
+++ app/modules/clients/services.py
@@ -12,3 +12,17 @@ def create_cliente(data: ClienteCreate):
     }
 
     return cliente_teste
+
+def getCliente_paginate(page: int = 1, per_page: int = 10):
+    total_simulado = 50
+    clientes_simulados = [
+        {"codcliente": 1, "cliente": "Empresa A", "email": "contato@empresaa.com"},
+        {"codcliente": 2, "cliente": "Empresa B", "email": "contato@empresab.com"}
+    ]
+
+    return {
+        "data": clientes_simulados,
+        "total": total_simulado,
+        "page": page,
+        "per_page": per_page
+    }
