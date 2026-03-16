41fd5a24544767aaf0a68dadfe7ed05647d6c7ad
Matheus Marques <matheusmarquesportela@gmail.com>
2026-03-16 17:01:22 -0300

    Criação da rota DELETE de cliente

--- app/modules/clients/router.py
+++ app/modules/clients/router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from . import schemas, services
 from app.core.database import get_db
 
-router = APIRouter(prefix="/clientes", tags=["Clientes"])
+router = APIRouter(prefix="/clientes", tags=["Rotas de Clientes"])
 
 @router.post("/cadastrar", status_code=201)
 def create_client(cliente: schemas.ClienteCreate, db: Session = Depends(get_db)):
@@ -13,3 +14,10 @@ def create_client(cliente: schemas.ClienteCreate, db: Session = Depends(get_db))
 @router.get("/listar")
 def listar_clientes(page: int = 1, per_page: int = 10, db: Session = Depends(get_db)):
     return services.getCliente_paginate(db=db, page=page, per_page=per_page)
+
+@router.delete("/deletar/{codcliente}")
+def deletar_cliente(codcliente: int, db: Session = Depends(get_db)):
+    
+    sucesso = services.delete_cliente(db=db, cliente_id=codcliente)
+
+    return sucesso
