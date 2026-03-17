0c11513e7704d8ef07b308bde916dc06a3f22181
Matheus Marques <matheusmarquesportela@gmail.com>
2026-03-17 15:06:03 -0300

    Criação da opção de UPDATE do CRUD de clientes

--- app/modules/clients/services.py
+++ app/modules/clients/services.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
-from datetime import date
-from fastapi import HTTPException
+#from datetime import date
+#from fastapi import HTTPException
 
-from .schemas import ClienteCreate
+#from .schemas import ClienteCreate
 from app.modules.util import schemas, models as model_util
 from . import schemas, models
 
@@ -87,6 +87,85 @@ def getCliente_paginate(db: Session, page: int = 1, per_page: int = 10):
         "page": page,
         "per_page": per_page
     }
+
+def update_cliente(db: Session, cliente_id: int, cliente_dados: schemas.ClienteCreate):
+    
+    cliente_db = db.query(models.Cliente).filter(models.Cliente.codcliente == cliente_id).first()
+    
+    if not cliente_db:
+        return {
+            "code": 404,
+            "message": "Cliente não encontrado",
+            "success": False
+        }
+    
+    try:
+        cliente_db.cliente = cliente_dados.cliente
+        cliente_db.fantasia = cliente_dados.fantasia
+        cliente_db.obs = cliente_dados.obs
+        cliente_db.bloqueio = cliente_dados.bloqueio
+        cliente_db.motivo_bloq = cliente_dados.motivo_bloq
+        
+        if cliente_db.codendereco:
+            endereco_db = db.query(model_util.Endereco).filter(model_util.Endereco.codendereco == cliente_db.codendereco).first()
+            if endereco_db:
+                endereco_db.logradouro = cliente_dados.logradouro
+                endereco_db.numero = cliente_dados.numero
+                endereco_db.cep = cliente_dados.cep
+                endereco_db.bairro = cliente_dados.bairro
+                endereco_db.cidade = cliente_dados.cidade
+                endereco_db.uf = cliente_dados.uf
+                endereco_db.pais = cliente_dados.pais
+        else:
+            return {
+                "code": 404,
+                "message": "Endereço não encontrado",
+                "success": False
+            }
+        
+        if cliente_db.codtelefone:
+            contato_db = db.query(model_util.Contato).filter(model_util.Contato.codcontato == cliente_db.codtelefone).first()
+            if contato_db:
+                contato_db.telefone = cliente_dados.telefone
+                contato_db.celular = cliente_dados.celular
+                contato_db.email = cliente_dados.email
+                if hasattr(cliente_dados, 'email2'):
+                    contato_db.email2 = cliente_dados.email2
+        else: 
+            return {
+                "code": 404,
+                "message": "Contato não encontrado",
+                "success": False
+            }
+        
+        if cliente_db.tipopessoa == 'F':
+            fisico_db = db.query(models.ClienteFisico).filter(models.ClienteFisico.codcli == cliente_id).first()
+            if fisico_db:
+                fisico_db.cpf = cliente_dados.cpf
+                fisico_db.rg = cliente_dados.rg
+                fisico_db.dt_nascimento = cliente_dados.dt_nascimento
+        
+        elif cliente_db.tipopessoa == 'J':
+            juridico_db = db.query(models.ClienteJuridico).filter(models.ClienteJuridico.codcli == cliente_id).fisrt()
+            if juridico_db:
+                juridico_db.cnpj = cliente_dados.cnpj
+                juridico_db.ie = cliente_dados.inscricaoestadual
+                if hasattr(cliente_dados, 'dtabertura'):
+                    juridico_db.dtabertura = cliente_dados.dtabertura
+                    
+        db.commit()
+        db.refresh(cliente_db)
+        
+        return {
+            "code": 200,
+            "message":  "Cliente alterado com sucesso",
+            "success": True,
+            "data": cliente_db
+        }
+        
+    except Exception as e:
+        db.rollback()
+        raise e
 
 def delete_cliente(db: Session, cliente_id: int):
     
