2ae0bee763312fcbc751ecf6c2620507d66e4f7d
Matheus Marques <matheusmarquesportela@gmail.com>
2026-04-18 08:19:46 -0300

    Ajustes rotas de setor

--- app/modules/setor/services.py
+++ app/modules/setor/services.py
@@ -25,6 +25,47 @@ def create_sector(db: Session, dados: schemas.SetorCreate):
         db.rollback()
         raise HTTPException(status_code=401, detail=f"Erro ao cadastrar setor. ERRO => {str(e)}")
     
+def getSector_paginate(db: Session, page: int = 1, per_page: int = 10):
+
+    offset = (page - 1) * per_page
+    total_setores = db.query(models.Sector).count()
+    
+    setores_db = db.query(models.Sector).offset(offset).limit(per_page).all()
+
+    return {
+        "status": 200,
+        "message": "Listagem de setores cadastrados",
+        "setor": setores_db, 
+        "total": total_setores,
+        "page": page,
+        "per_page": per_page
+    }
+
+def inativar_sector(db: Session, codsetor: int):
+
+    setor_db = db.query(models.Sector).filter(models.Sector.codsetor == codsetor).first()
+
+    if not setor_db:
+        return {    
+            "status": 404,
+            "message": "Setor não encontrado",
+            "success": False
+        }
+    
+    try:
+        setor_db.status = 'I'
+        db.commit()
+        db.refresh(setor_db)
+
+        return {
+            "status": 200,
+            "message": "Setor inativado com sucesso",
+            "data": setor_db
+        }
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=401, detail=f"Erro ao inativar o setor. ERRO => {str(e)}")
+
 def delete_sector(db: Session, codsetor: int):
     try:
         sector = db.query(models.Sector).filter(models.Sector.codsetor == codsetor).first()
