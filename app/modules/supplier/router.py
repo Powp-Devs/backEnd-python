a63488aa4c78f9b938ea536a4f9b904d1efb6790
Victor Manoel <silvavictormoraes@gmail.com>
2026-04-20 14:40:17 -0300

    Finalizei as implementações das novas rotas da APi. Criei a rota da att do cadastro de fornecedor PUT, a listagem com paginação GET e a atualização de dados PUT.

--- app/modules/supplier/router.py
+++ app/modules/supplier/router.py
@@ -17,3 +17,11 @@ def delete_fornec(codfornec: int, db: Session = Depends(get_db)):
 @router.get("/listar", summary="Listar todos os fornecedores", description="Rota para listar todos os fornecedores")
 def list_fornec(page: int = 1, per_page: int = 10, db: Session = Depends(get_db)):
     return services.list_fornecedor(db=db, page=page, per_page=per_page)
+
+@router.put("/atualizar/{codfornec}", summary="Atualizar um fornecedor", description="Rota para atualizar os dados de um fornecedor")
+def update_fornec(codfornec: int, fornecedor: schemas.FornecedorUpdate, db: Session = Depends(get_db)):
+    return services.update_fornecedor(db=db, codfornec=codfornec, fornecedor_update=fornecedor)
+
+@router.get("/listar", summary="Listar empregados com paginação", description="Rota para listar empregados com paginação")
+def list_employee(page: int = 1, per_page: int = 10, db: Session = Depends(get_db)):
+    return services.getEmployee_paginate(db=db, page=page, per_page=per_page)
--- app/modules/supplier/schemas.py
+++ app/modules/supplier/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, EmailStr, Field, constr
 from datetime import date
 from typing import Literal, Optional
 
+
 class FornecedorCreate(BaseModel):
     #Campos PWFORNECEDOR
     fornecedor: str = Field(..., max_length=255)
@@ -30,4 +31,35 @@ class FornecedorCreate(BaseModel):
     telefone: str = Field(None, max_length=20)
     celular: str = Field(None, max_length=20)
     email: EmailStr = Field(None, max_length=150)
+    email2: Optional[EmailStr] = Field(None, max_length=150)
+
+
+class FornecedorUpdate(BaseModel):
+    #Campos PWFORNECEDOR
+    fornecedor: Optional[str] = Field(None, max_length=255)
+    fantasia: Optional[str] = Field(None, max_length=255)
+    cnpj: Optional[str] = Field(None, max_length=14)
+    inscricaoestadual: Optional[str] = Field(None, max_length=20)
+    tipopessoa: Optional[Literal['F', 'J']] = None
+    dtcadastro: Optional[date] = None
+    obs: Optional[str] = Field(None, max_length=255)
+    bloqueio: Optional[str] = Field(None, min_length=1, max_length=1)
+    motivo_bloqueio: Optional[str] = None
+    dtbloqueio: Optional[date] = None
+    nome_representante: Optional[str] = Field(None, max_length=255)
+    cpf_representante: Optional[str] = Field(None, max_length=11)
+    
+    #Campos PWENDERECO
+    cep: Optional[str] = Field(None, max_length=9)
+    logradouro: Optional[str] = Field(None, max_length=255)
+    numero: Optional[str] = Field(None, max_length=10)
+    bairro: Optional[str] = Field(None, max_length=80)
+    cidade: Optional[str] = Field(None, max_length=50)
+    uf: Optional[str] = Field(None, min_length=2, max_length=2)
+    pais: Optional[str] = Field(None, min_length=2, max_length=2)
+    
+    #Campos PWCONTATO
+    telefone: Optional[str] = Field(None, max_length=20)
+    celular: Optional[str] = Field(None, max_length=20)
+    email: Optional[EmailStr] = Field(None, max_length=150)
     email2: Optional[EmailStr] = Field(None, max_length=150)
