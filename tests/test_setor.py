3ca90c500fa554875b813a7ad4733904ee0f1435
Matheus Marques <matheusmarquesportela@gmail.com>
2026-04-29 22:37:18 -0300

    Ajuste do teste de setor

--- tests/test_setor.py
+++ tests/test_setor.py
@@ -1,14 +1,18 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool 
 
 from app.main import app
 from app.core.database import get_db, Base
+from app.modules.setor import models
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool 
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -26,7 +30,6 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 def test_create_setor():
-
     dados_new = {
         "setor": "Tecnologia Teste",
         "status": "A"
@@ -37,7 +40,7 @@ def test_create_setor():
     assert response.status_code == 201
 
     json_response = response.json()
-    assert json_response["message"] == "setor cadastrado com sucesso"
+    assert json_response["message"] == "Setor cadastrado com sucesso"
     assert json_response["data"]["setor"] == "Tecnologia Teste"
     assert json_response["data"]["status"] == "A"
     assert "codsetor" in json_response["data"]
